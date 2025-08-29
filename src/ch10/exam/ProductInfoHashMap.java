6ea6623985bf19d6a7c5e9d04b7b4f33884888f2
am_o_o <am_o_o@naver.com>
2025-08-29 12:50:58 +0900

    커밋커밋

---
+++ src/ch10/exam/ProductInfoHashMap.java
@@ -0,0 +1,114 @@
+package ch10.exam;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.Vector;
+
+import ch07.exam.Book;
+import ch07.exam.CompackDisk;
+import ch07.exam.ConversationBook;
+import ch07.exam.Product;
+
+public class ProductInfoHashMap {
+	static HashMap<Integer, Product> map;
+	static Scanner sc;
+	static int cnt=0;
+	
+	static void input() {
+		while(true) {
+			System.out.print("상품의 종류를 선택:1 책|2 앨범|3 회화책|4 입력종료>>");
+			int num=sc.nextInt();
+			if(num==4)
+				break;
+			System.out.print("상품설명:>>");
+			sc.nextLine();
+			String comment=sc.nextLine();
+			System.out.print("생산자>>");
+			String productor=sc.next();
+			System.out.print("가격>>");
+			int price=sc.nextInt();
+			
+			switch(num) {
+			case 1: 
+				System.out.print("책제목>>");
+				String title=sc.next();
+				System.out.print("저자>>");
+				String author=sc.next();
+				map.put(++cnt, new Book(cnt,comment,productor,price,title,author));
+				break;
+			case 2:
+				System.out.print("앨범제목>>");
+				String albumTitle=sc.next();
+				System.out.print("가수>>");
+				String singer=sc.next();
+				map.put(++cnt, new CompackDisk(cnt, comment,productor,price,albumTitle, singer));
+				break;
+			case 3:
+				System.out.print("책제목>>");
+				title=sc.next();
+				System.out.print("저자>>");
+				author=sc.next();
+				System.out.print("언어>>");
+				String language=sc.next();
+				map.put(++cnt, new ConversationBook(cnt,comment,productor,price,title,author,language));
+				break;
+			default:
+					System.out.println("입력오류");
+			}
+		}
+		System.out.println("데이터입력 종료");
+		
+	}
+	static void search() {
+		while(true) {
+			System.out.print("상품아이디>>");
+			int key=sc.nextInt();
+			if(key==0)
+				break;
+			boolean flag=map.containsKey(key);
+			if(flag)
+				map.get(key).display();
+			else
+				System.out.println("찾는 값이 없습니다.");
+		}
+		System.out.println("검색종료");
+		
+	}
+	static void allShow() {
+		Set<Integer> keys=map.keySet();
+		Iterator<Integer> it=keys.iterator();
+		
+		while(it.hasNext()) {
+			Integer key=it.next();
+			map.get(key).display();;
+		}
+	}
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		map=new HashMap<>();
+		sc=new Scanner(System.in);
+				
+		boolean flag=false;
+		while(true) {
+			System.out.println("1 상품추가|2 상품조회|3 모든 상품조회|4 끝내기");
+			int num=sc.nextInt();
+			switch(num) {
+			case 1: input(); break;
+			case 2: search();break;
+			case 3: allShow();break;
+			case 4: flag=true;break;
+			default:System.out.println("입력오류");
+			}
+			if(flag)
+				break;
+		}
+		System.out.println("프로그램 종료");
+
+	}
+
+}
---
+++ src/ch10/exam/StudentHashEx.java
@@ -0,0 +1,106 @@
+package ch10.exam;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class StudentHashEx {
+	static Scanner sc;
+	static HashMap<String, Student> map;
+
+	static void rankPro() {
+		Set<String> keys = map.keySet();
+		Iterator<String> it1 = keys.iterator();
+
+		while (it1.hasNext()) {
+			String key1 = it1.next();
+			Iterator<String> it2 = keys.iterator();
+			while (it2.hasNext()) {
+				String key2 = it2.next();
+				map.get(key1).setRank(map.get(keys));
+			}
+		}
+	}
+
+	static void input() {
+		while (true) {
+			int score[] = new int[3];
+
+			System.out.println("이름, 학번, 전공, 성적3개 입력>> ");
+			String name = sc.next();
+			if (name.equals("exit"))
+				break;
+			int sno = sc.nextInt();
+			String major = sc.next();
+			for (int i = 0; i < score.length; i++) {
+				score[i] = sc.nextInt();
+			}
+//			Student s= new Student(name, sno, major, score);
+			map.put(name, new Student(name, sno, major, score));
+		}
+		rankPro();
+		System.out.println("데이터 입력 완료");
+	}
+
+	static void search() {
+		while (true) {
+			System.out.print("이름>>");
+			String name = sc.next();
+			if (name.equals("exit"))
+				break;
+			boolean flag = map.containsKey(name);
+			if(flag)
+				map.get(name).display();
+			else 
+				System.out.println("해당 이름이 없습니다.");
+		}
+		System.out.println("검색종료");
+	}
+
+	static void allShow() {
+		Set<String> keys = map.keySet();
+		Iterator<String> it = keys.iterator();
+		
+		while(it.hasNext()) {
+			String key = it.next();
+			System.out.println(map.get(key));
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		map = new HashMap<String, Student>();
+		sc = new Scanner(System.in);
+		boolean flag = false;
+
+		while (true) {
+			System.out.println("입력(1), 조회(2), 전체보기(3), 종료(4)");
+			int num = sc.nextInt();
+
+			switch (num) {
+			case 1:
+				input();
+				break;
+			case 2:
+				search();
+				break;
+			case 3:
+				allShow();
+				break;
+			case 4:
+				flag = true;
+				break;
+			default:
+				System.out.println("입력오류");
+			}
+
+			if (flag) {
+				break;
+			}
+		}
+		System.out.println("종료합니다.");
+	}
+
+}
